50d96d1c8629880a465d07477429a64c38a11d82
didallorto <diogodallorto@gmail.com>
2019-09-14 02:54:46 -0300

    myflaskapp.sql

---
+++ myflaskapp.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE myflaskapp
+use myflaskapp
+
+CREATE TABLE users(
+	id int identity(3,3)
+	, name varchar(30)
+	, email varchar(50)
+	, username varchar(10)
+	, [password] varchar(15)
+	, register_date datetime not null
+	, constraint pk_id primary key(id)
+);
+go
+
+alter table users add register_date datetime not null 
+
+
+
+
+INSERT dbo.users(id, name, email, username, [password], register_date)
+    VALUES (1, 'iotontos', 'iotontos@iotontos.com.br', 'masterbr', 999999, GETDATE())
+GO  
+
+INSERT INTO dbo.users( name, email, username, [password], register_date)
+    VALUES ('masterotontos@iotontos.com.br', 'masterbr', 999999, GETDATE())
+	go
+
+INSERT INTO dbo.users( name, email, username, [password], register_date)
+    VALUES ('flask', 'flask@sqlserver.db', 'flask', 'dasrmater2019', GETDATE())
+	go
