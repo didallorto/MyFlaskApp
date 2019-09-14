17819fb9ba580f9a09e69ec35ea1291e3b2402a8
didallorto <diogodallorto@gmail.com>
2019-09-14 20:53:24 -0300

    Form with validators and route is ok

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
-from data import Articles
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from data import Articles  
+#from flash_ ver como importar sql
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
@@ -31,6 +34,24 @@ def article(id):
 def photos():
     return render_template('photos.html')
 
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password do not match!')      
+    ])
+    confirm = PasswordField('Confirm Password')
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        form = RegisterForm(request.form)
+        if request.method == ['POST'] and form.validate():
+            return render_template('register.html')
+        return render_template('register.html', form=form)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
