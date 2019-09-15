6a23cb21e97ce67f68bcb99524cdaf5f49b261f9
didallorto <diogodallorto@gmail.com>
2019-09-14 23:17:51 -0300

    ajustar banco e validadores

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from data import Articles  
-#from flash_ ver como importar sql
+from data import Articles
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+import hashlib
 from passlib.hash import sha256_crypt
+#import para sqlserver
+from urllib.parse import quote_plus
+import pyodbc
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
+
+parametros = (
+# Driver que será utilizado na conexão#
+    'DRIVER={ODBC Driver 17 for SQL Server}', 
+# IP ou nome do servidor.#
+    'SERVER=DARKMATTER-PC\DARKMATER;', 
+# Porta#
+    'PORT=1433;', 
+# Banco que será utilizado.#
+    'DATABASE=pythonSQL;', 
+# Nome de usuário.#
+    'UID=python;', 
+# Senha/Token.#
+    'PWD=123456'
+)
+
 
 
 Articles = Articles()
