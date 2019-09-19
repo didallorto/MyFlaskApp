d2c224584ac04d6de9a20d9aab60c502fbfcca9f
@Buenomaurilio <mauriliodsb@gmail.com>
2019-09-18 22:06:47 -0300

    testeconnect

--- app.py
+++ app.py
@@ -2,52 +2,24 @@ from flask import Flask, render_template, flash, redirect, url_for, session, req
 from data import Articles
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+import hashlib
 #import para sqlserver
 from urllib.parse import quote_plus
 import pyodbc
-from sqlalchemy import create_engine
-from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 
-#conexão
+app.config['MYSQL_HOST'] = 'Localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '147852'
+app.config['MYSQL_DB'] = 'mydb'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-from urllib.parse import quote_plus
-
-import pyodbc
-from sqlalchemy import create_engine
-
-# Verificar se os drivers estão instalados.
-# print([x for x in pyodbc.drivers() if x.startswith('ODBC')])
-# print(pyodbc.drivers())
-
-parametros = (
-    # Driver que será utilizado na conexão
-    'DRIVER={SQL Server};'
-    # IP ou nome do servidor.
-    'SERVER=127.0.0.1;'
-    # Porta
-    'PORT=1433;'
-    # Banco que será utilizado.
-    'DATABASE=myflaskapp;'
-    # Nome de usuário.
-    'UID=sa;'
-    # Senha/Token.
-    'PWD=147852')
-
-url_db = quote_plus(parametros)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc:///?odbc_connect=%s'% url_db
-
-db = SQLAlchemy(app)
-
-
-
-
-
-
+mysql = MySQL(app)
 Articles = Articles()
-
 
 @app.route('/')
 def index():
@@ -75,26 +47,37 @@ def photos():
 
 @app.route('/errormessage')
 def errormessage():
-    return render_template('errormessage.html')
+    return render_template('errormessage.html', current_time=datetime.utcnow())
 
 
 class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
+    name = StringField('Name', [validators.DataRequired(), validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.DataRequired(), validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.DataRequired(), validators.Length(min=6, max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Password do not match!')      
-    ])
-    confirm = PasswordField('Confirm Password')
+        validators.EqualTo('confirm', message='Password do not match!')])
+    confirm = PasswordField('Confirm Password', [validators.DataRequired()])
 
-    @app.route('/register', methods=['GET', 'POST'])
-    def register():
-        form = RegisterForm(request.form)
-        if request.method == ['POST'] and form.validate():
-            return render_template('register.html')
-        return render_template('register.html', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == ['POST'] and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+            
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        mysql.connection.commit()            
+        cur.close()
+        redirect(url_for('index'))
+      
+    return render_template('register.html', form=form)
+        
     
-
 if __name__ == '__main__':
     app.run(debug=True)
+    app.secret_key = '147852'
+    
